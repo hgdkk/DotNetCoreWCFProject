f996f9d70ba0e6ccf0fe70e0a2e6302b4927f002
Halil Gedik <halilgedik06@gmail.com>
2022-01-25 13:54:31 +0300

    init

---
+++ DotNetCoreWCFProject/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCoreWCFProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreWCFProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            DataService.DataServiceClient service = new DataService.DataServiceClient();
+            Data d = new Data();
+            d.DataValue = await service.GetDataAsync(5);
+            ViewData["DataModel"] = d;
+            return View();
+        }
+
+       
+    }
+}
---
+++ DotNetCoreWCFProject/Models/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWCFProject.Models
+{
+    public class Data
+    {
+        public string DataValue { get; set; }
+    }
+}
